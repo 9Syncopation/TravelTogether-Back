eb5e0179dc5f62b2ee429d3b58917be68976824f
9syncopation <9syncopation@gmail.com>
2020-07-12 03:43:04 +0300

    deployement setup

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+const {PASSWORD_ATLAS , USERNAME_ATLAS } = require('./details-helper')
+
+module.exports = {
+//   "dbURL":`mongodb+srv://${USERNAME_ATLAS}:${PASSWORD_ATLAS}@cluster0-sbs2x.mongodb.net/test?retryWrites=true&w=majority`
+  "dbURL":`mongodb+srv://UserTravel:UserTravel33@cluster0-sbs2x.mongodb.net/test?retryWrites=true&w=majority`
+}
