1225259d38c0509a1aafc5b526ef3996774cc8d7
9syncopation <9syncopation@gmail.com>
2020-07-12 05:12:06 +0300

    server.js line 41

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/user', userRoutes)
 app.use('/api/trip', tripRoutes)
 connectSockets(io)
 
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('public'));
 }
 
